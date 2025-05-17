bdd0458348409474bbda8eebde8c7f264a37fc77
youdi <mehdi.youssef97@gmail.com>
2025-05-16 23:06:35 -0400

    refresh

--- xHunter/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = "0.1.0"
-__all__ = ['cli', 'modules']
-
-# Package-level documentation
-__doc__ = """
-xHunter - Network Security Scanner
-A comprehensive toolkit for penetration testers
-"""
--- xHunter/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-from .network import NetworkScanner
-__all__ = ['NetworkScanner']
-
-# Package-level documentation
-__doc__ = """
-xHunter - Network Security Scanner
-A comprehensive toolkit for penetration testers
-"""
